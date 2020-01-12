d155a38fba88f2db0ff0141656f7f6e8048a371e
Clément Contini <vilisseranen@gmail.com>
2020-01-11 19:01:57 -0500

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Deploy on server
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no travis-deploy@clemissa.info -P 274 dist/* /var/www/castellers-ui/
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 274 dist/* travis-deploy@clemissa.info:/var/www/castellers-ui/
